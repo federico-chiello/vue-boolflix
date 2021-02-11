c825f15dedba16ddfb72ba5990015e13c03cfe25
Fede <chiellofederico@gmail.com>
2021-02-11 12:47:59 +0100

    inserimento stelle

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ var app = new Vue({
     apikey:'2440848574e245be0482427477ace718',
     lang:'it-IT',
     movies:[],
-    seriesTv: []
+    seriesTv: [],
+    starMovie: [],
+    starSeriesTv: []
   },
   methods:{
     searchMovieTvSeries(){
@@ -51,6 +53,15 @@ var app = new Vue({
 // 2. Titolo Originale
 // 3. Lingua
 // 4. Voto
+// ******* Milestone 2: ********
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
+// permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+// lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze
+// piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della
+// nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
+// nazione ritornata dall’API (le flag non ci sono in FontAwesome)
 // ******** Milestone 3: **********
 // In questa milestone come prima cosa aggiungiamo la copertina del film o della serie al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
 // Dovremo prendere quindi l’URL base delle immagini di TMDB:
